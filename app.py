08fdd0f520a19d124892bf6c21bcd823812e422e
KielRodriguez <kr.rdz.20@gmail.com>
2020-02-09 17:03:58 -0600

    full example todo api

--- app.py
+++ app.py
@@ -1,38 +1,60 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-todos = {}
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
 
-class TodoSimple(Resource):
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('task', type=str, help="taks is required")
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
     def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+    
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
 
     def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
 
-class Todo1(Resource):
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
     def get(self):
-        # Default to 200 OK
-        return {'task': 'Hello world'}
+        return TODOS
+    
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
 
-class Todo2(Resource):
-    def get(self):
-        # Set the response code to 201
-        return {'task': 'Hello world'}, 201
+##
+## Actually setup the Api resource here
+##
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
 
-class Todo3(Resource):
-    def get(self):
-        # Default to 200 OK and return custom headers
-        return {'task': 'Hello world'}, 201, {'Etag': 'some-opaque-string'}
-
-api.add_resource(TodoSimple, '/<string:todo_id>')
-api.add_resource(Todo1, '/todo1')
-api.add_resource(Todo2, '/todo2')
-api.add_resource(Todo3, '/todo3')
 
 if __name__ == '__main__':
     app.run(debug=True)
